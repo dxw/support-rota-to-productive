b9a1d608ca728732d5726ee3cb2c0cf7df7f4349
meyric <meyric@dxw.com>
2021-05-07 13:21:27 +0100

    support_rotation factory can use build
    We don't need to persist the faek employee object so we user `build`
    over `create` - this saves us a database call.

--- spec/factories/support_rotation.rb
+++ spec/factories/support_rotation.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :support_rotation, class: SupportRotaToProductive::SupportRotation do
-    date { '2021-03-01'}
-    employee { FactoryBot.create(:employee) }
+    date { "2021-03-01" }
+    employee { build(:employee) }
 
     skip_create
   end
